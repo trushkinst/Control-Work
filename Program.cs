3931fdaeb14b8ca94e10ca76f0035162381c26be
Stas <trushkin.st@mail.ru>
2022-10-15 13:10:43 +0300

    New

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string[] in_mas = { "DayOfWeek", "devor", "del", "mar", "sam", "prokm", "end" };
+
+string[] SearchForThrdCharString(string[] in_mas)
+{
+    string[] out_mas = new string[1];
+    int j = 0;
+    for (int i = 0; i < in_mas.Length; i++)
+    {
+        if (in_mas[i].Length <= 3)
+        {
+            Array.Resize(ref out_mas, 1 + j);
+            out_mas[j] = in_mas[i];
+            j++;
+        }
+    }
+    return out_mas;
+}
+System.Console.WriteLine(String.Join("|", SearchForThrdCharString(in_mas)));
